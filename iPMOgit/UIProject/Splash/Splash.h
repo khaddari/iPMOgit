5f2a9b6875b5e68cb16a12523d3d3cb69ad03d6b
Ayoub Korkot <ayoub@majjane.net.ma>
2013-07-12 09:23:44 +0100

    addd file

---
+++ iPMOgit/UIProject/Splash/Splash.h
@@ -0,0 +1,13 @@
+//
+//  Splash.h
+//  iPMOgit
+//
+//  Created by Ayoub Korkot on 12/07/2013.
+//  Copyright (c) 2013 Majjane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Splash : UIViewController
+
+@end
---
+++ iPMOgit/UIProject/Splash/Splash.m
@@ -0,0 +1,38 @@
+//
+//  Splash.m
+//  iPMOgit
+//
+//  Created by Ayoub Korkot on 12/07/2013.
+//  Copyright (c) 2013 Majjane. All rights reserved.
+//
+
+#import "Splash.h"
+
+@interface Splash ()
+
+@end
+
+@implementation Splash
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
