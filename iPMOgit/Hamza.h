58d81c38774c7a5c22b248eb85afe3de7bfd6b1d
Ayoub Korkot <ayoub@majjane.net.ma>
2013-07-12 09:34:54 +0100

    hamza

---
+++ iPMOgit/Hamza.h
@@ -0,0 +1,13 @@
+//
+//  Hamza.h
+//  iPMOgit
+//
+//  Created by Ayoub Korkot on 12/07/2013.
+//  Copyright (c) 2013 Majjane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Hamza : UIViewController
+
+@end
---
+++ iPMOgit/Hamza.m
@@ -0,0 +1,38 @@
+//
+//  Hamza.m
+//  iPMOgit
+//
+//  Created by Ayoub Korkot on 12/07/2013.
+//  Copyright (c) 2013 Majjane. All rights reserved.
+//
+
+#import "Hamza.h"
+
+@interface Hamza ()
+
+@end
+
+@implementation Hamza
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
